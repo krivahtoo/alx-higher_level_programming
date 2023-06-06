11218d60caa47028f30183830860947cbfe8bc81
Noah Too <krivahtoo@gmail.com>
2023-06-06 15:39:14 +0300

    fix: tab indentation

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+if (number < 0):
+    last = -last
+
+msg = "and is less than 6 and not 0"
+if (last > 5):
+    msg = "and is greater than 5"
+elif (last == 0):
+    msg = "and is 0"
+
+print(f"Last digit of {number:d} is {last:d} and is {msg}")
