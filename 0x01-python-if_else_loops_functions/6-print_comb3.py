4768d19144b25bda676e0974cf75d84e61b97740
Noah Too <krivahtoo@gmail.com>
2023-06-06 20:45:47 +0300

    fix: task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 
 for y in range(0, 8):
     for x in range(0, 10):
-        if (x > y or (x == 0 and y == 0)):
+        if (x > y):
             print("{0:d}{1:d}, ".format(y, x), end='')
 print("{}".format(89))
