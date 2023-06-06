e236c075ec45b4b5d4e56a4ed61f6e1c47c274b9
Noah Too <krivahtoo@gmail.com>
2023-06-06 06:48:31 +0300

    Task 10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdint.h>
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *node1, *node2;
+
+	node1 = list;
+	if (list->next && list->next->next)
+		node2 = list->next->next;
+	else
+		return (0);
+	while (node1->next && (node2->next && node2->next->next))
+	{
+		if ((uintptr_t)node1 == (uintptr_t)node2)
+			return (1);
+		node2 = node2->next->next;
+		node1 = node1->next;
+	}
+	return (0);
+}
