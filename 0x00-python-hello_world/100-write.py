addfc40555ea1eac4843135898bf32d466718abe
Noah Too <krivahtoo@gmail.com>
2023-06-06 07:06:24 +0300

    fix: 0x00 tasks

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-from sys import write, exit
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n".encode())
+
+from sys import exit
+from os import write
+
+write(2, "and that piece of art is useful\
+ - Dora Korpar, 2015-10-19\n".encode())
+
 exit(1)
