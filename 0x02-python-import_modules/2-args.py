06ba7ee4d97a5ccaa1c35ce0fc41e6563089fe9e
Noah Too <krivahtoo@gmail.com>
2023-06-08 22:35:32 +0300

    fix: 2-args task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,10 @@ from sys import argv
 
 if __name__ == "__main__":
     n = len(argv)
-    print(f"{n-1} arguments", end="")
+    print(f"{n-1} argument", end="")
 
+    if (not n == 2):
+        print("s", end="")
     if (n < 2):
         print(".")
     else:
