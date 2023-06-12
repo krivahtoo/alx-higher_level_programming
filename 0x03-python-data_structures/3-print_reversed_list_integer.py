dc90948177b910ccda82ada58bed4900b5bc0d62
Noah Too <krivahtoo@gmail.com>
2023-06-12 15:07:46 +0300

    fix: Tasks 2-4

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    str = "{:d}"
     for num in reversed(my_list):
-        print("{:d}".format(num))
+        print(str.format(num))
 
 
 if __name__ == "__main__":
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     my_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
 
