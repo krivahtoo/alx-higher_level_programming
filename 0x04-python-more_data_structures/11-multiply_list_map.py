5acb230cba14edcd74ecd50521e03e6786e73fe4
Noah Too <krivahtoo@gmail.com>
2023-06-15 09:24:25 +0300

    fix: task 11 line count

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-# returns a list with all values multiplied by a number
-# without using any loops.
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 6]
-    new_list = multiply_list_map(my_list, 4)
-    print(new_list)
-    print(my_list)
