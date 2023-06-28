d67e31b1fe77bdfc61fb18b3c5eec5e3ccfded32
Noah Too <krivahtoo@gmail.com>
2023-06-28 18:10:11 +0300

    fix: task 0x05 exeption handler

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except Exception as e:
         print(f"Exception: {e}", file=stderr)
         return False
     return True
@@ -25,6 +25,11 @@ if __name__ == "__main__":
         print("{} is not an integer".format(value))
 
     value = "School"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = {89}
     has_been_print = safe_print_integer_err(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
