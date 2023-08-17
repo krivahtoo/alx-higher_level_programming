75fd8e4b5248844adbe935d071f24500464c1e7a
Noah Too <krivahtoo@gmail.com>
2023-08-17 12:36:41 +0300

    Update all sql files

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the score average of all records in the table second_table of the database hbtn_0c_0 in MySQL server.
+-- The result column name is average.
+SELECT AVG(score) AS average FROM second_table;
