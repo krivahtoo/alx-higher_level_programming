c59141d01a3534512967439bed3e30db23124614
Noah Too <krivahtoo@gmail.com>
2024-07-30 23:56:05 +0300

    Add Task 3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Failed to fetch movie. Status code: ${response.statusCode}`);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
